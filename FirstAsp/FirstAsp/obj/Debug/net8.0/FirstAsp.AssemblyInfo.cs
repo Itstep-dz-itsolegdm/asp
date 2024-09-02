b26e57c2dce6c3e00874cdba395014c210f40063
ItsOlegDm <itsolegdm@gmail.com>
2024-09-02 19:42:50 +0300

    fix

--- FirstAsp/FirstAsp/obj/Debug/net8.0/FirstAsp.AssemblyInfo.cs
+++ FirstAsp/FirstAsp/obj/Debug/net8.0/FirstAsp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstAsp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1584ac4022ce7b70b912daa1d4353bb543b7b243")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9413fcdb15c46a6b647c48c247ce63ee4e96a05")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstAsp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstAsp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
